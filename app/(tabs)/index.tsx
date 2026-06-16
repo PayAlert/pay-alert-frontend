36f0089d0bbca120f88ec53fc82d91ff41f3660d
Nakul <nakulsinghsisodia07@gmail.com>
2026-06-16 17:45:51 +0530

    created currency formatter

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,20 +9,7 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function App() {
     return (
         <SafeAreaView className="flex-1 items-center justify-center bg-background">
-            <Text className="text-5xl font-bold text-primary">Home</Text>
-            <Text className="text-5xl font-sans-bold text-teal-700">Home</Text>
-            <Link href="/onboarding" className="mt-4 px-3 py-3 bg-black text-white rounded-md" >Go to Onboarding</Link>
-            <Link href="/(auth)/sign-in" className="mt-4 px-3 py-3 bg-black text-white rounded-md" >Go to Sign In</Link>
-            <Link href="/(auth)/sign-up" className="mt-4 px-3 py-3 bg-black text-white rounded-md" >Go to Sign Up</Link>
 
-            <Link href="/subscriptions/spotify" className="mt-4 px-3 py-3 bg-black text-white rounded-md">Spotify</Link>
-
-            <Link href={
-                {
-                    pathname: '/subscriptions/[id]',
-                    params: {id: 'claude'}
-                }
-            } className="mt-4 px-3 py-3 bg-black text-white rounded-md">Claude</Link>
         </SafeAreaView>
     );
 }
