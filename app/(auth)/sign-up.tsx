4a47e518112332981bc368888bfb0609efa13bb9
Nakul Singh Sisodia <153912261+nakulsinghsisodia@users.noreply.github.com>
2026-06-17 20:48:57 +0530

    Update sign-up.tsx

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
-
+// will use clerk
 const SignUp = () => {
   return (
     <View className='flex-1 items-center'>
